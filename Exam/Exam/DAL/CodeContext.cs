d0b095046c9daf20a797c7ce7ac2ea86d3ef5028
Tran Cong Vu (APTECH HN) <vutcth1807074@fpt.edu.vn>
2020-04-03 20:16:28 +0700

    Exam

---
+++ Exam/Exam/DAL/CodeContext.cs
@@ -0,0 +1,19 @@
+﻿using Exam.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Exam.DAL
+{
+    public class CodeContext : DbContext
+    {
+        public CodeContext():base("CodeContext")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Exam/Exam/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierId { get; set; }
+        public int CategoryID { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int  UnitStock { get; set; }
+        public bool UnitOnOrder { get; set; }
+        public int ReOrderLever { get; set; }
+        public bool Discontuned { get; set; }
+
+        public Category Category { get; set; }
+        
+
+    }
+}
